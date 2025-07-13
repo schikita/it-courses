44496633d92021953ef3c6fa7c67a8a59f9981aa
Zortea Roman <romeozortea@gmail.com>
2025-07-13 17:22:12 +0200

    lessons, enrollments, courses

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -1,12 +1,92 @@
 from rest_framework import serializers
-from .models import User, Image
+from .models import (
+    Image, User, Course, Lesson, Enrollment, CourseImage
+)
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'full_name', 'role', 'google_id', 'telegram_id', 'is_active', 'is_staff']
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ['id', 'title', 'image', 'uploaded_at']
+        fields = '__all__'
+
+
+# ------------------- User -------------------
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'full_name', 'role')
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    avatar = ImageSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'full_name', 'role', 'avatar', 'google_id', 'telegram_id')
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'full_name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
+
+
+# ------------------- Course -------------------
+
+class CourseSerializer(serializers.ModelSerializer):
+    teacher = UserListSerializer(read_only=True)
+    image = ImageSerializer(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    teacher = UserListSerializer(read_only=True)
+    image = ImageSerializer(read_only=True)
+    lessons = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_lessons(self, obj):
+        return LessonSerializer(obj.lessons.all(), many=True).data
+
+
+# ------------------- Lesson -------------------
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
+# ------------------- Enrollment -------------------
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    course = CourseSerializer(read_only=True)
+    course_id = serializers.PrimaryKeyRelatedField(
+        queryset=Course.objects.all(), source='course', write_only=True
+    )
+
+    class Meta:
+        model = Enrollment
+        fields = ('id', 'user', 'course', 'course_id', 'progress', 'is_completed', 'enrolled_at')
+        read_only_fields = ('user', 'enrolled_at')
+
+
+# ------------------- CourseImage -------------------
+
+class CourseImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseImage
+        fields = '__all__'
--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,13 +1,103 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Image, User
-from .serializers import ImageSerializer, UserSerializer
+from rest_framework import viewsets, permissions, generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import (
+    Image, User, Course, Lesson, Enrollment, CourseImage
+)
+from .serializers import (
+    ImageSerializer,
+    UserListSerializer, UserDetailSerializer, UserCreateSerializer,
+    CourseSerializer, CourseDetailSerializer,
+    LessonSerializer,
+    EnrollmentSerializer,
+    CourseImageSerializer,
+)
+
+
+# -------------------- Users --------------------
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return UserDetailSerializer
+        if self.action == 'create':
+            return UserCreateSerializer
+        return UserListSerializer
+
+
+class CurrentUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserDetailSerializer(request.user)
+        return Response(serializer.data)
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+# -------------------- Images --------------------
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# -------------------- Courses --------------------
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return CourseDetailSerializer
+        return CourseSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(teacher=self.request.user)
+
+    @action(detail=True, methods=['get'], url_path='lessons')
+    def lessons(self, request, pk=None):
+        course = self.get_object()
+        lessons = course.lessons.all()
+        serializer = LessonSerializer(lessons, many=True)
+        return Response(serializer.data)
+
+
+# -------------------- Lessons --------------------
+
+class LessonViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+# -------------------- Enrollments --------------------
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Enrollment.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+# -------------------- Course Images --------------------
+
+class CourseImageViewSet(viewsets.ModelViewSet):
+    queryset = CourseImage.objects.all()
+    serializer_class = CourseImageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
